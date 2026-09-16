a653fa5ea7e1b6632434144f7bbe967b5205c2f9
Nenad <nenadradomirovic.nerf@gmail.com>
2026-09-16 15:47:11 +0200

    Add CRUD operations

--- BudgetBook/Data/Models/Transaction.cs
+++ BudgetBook/Data/Models/Transaction.cs
@@ -6,12 +6,13 @@ namespace BudgetBook.Data.Models
     public class Transaction
     {
         public int Id { get; set; }
+        [Range(typeof(decimal), "0,01", "999999999999", ErrorMessage = "Amount must be greater than zero.")]
         public decimal Amount { get; set; }
         public DateTime BookingDate { get; set; }
         public TransactionType Type { get; set; }
         public string Description { get; set; } = string.Empty;
 
-       
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category.")]
         public int CategoryId { get; set; }
         public Category Category { get; set; } = null!;
 
